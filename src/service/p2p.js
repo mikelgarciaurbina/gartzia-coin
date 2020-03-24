575cac93e71951fc9212b559feb5489ee7c962e9
mikelgarciaurbina <mikelgarciaurbina@gmail.com>
2020-03-24 11:49:48 +0100

    Add broadcasting between the nodes

--- src/service/p2p.js
+++ src/service/p2p.js
@@ -2,6 +2,7 @@ import WebSocket from 'ws';
 
 const { P2P_PORT = 5000, PEERS } = process.env;
 const peers = PEERS ? PEERS.split(',') : [];
+const MESSAGE = { BLOCKS: 'blocks' };
 
 class P2PService {
   constructor(blockchain) {
@@ -23,8 +24,27 @@ class P2PService {
 
   onConnection(socket) {
     console.log('[ws:socket] connected.');
+    const { blockchain: { blocks } } = this;
 
     this.sockets.push(socket);
+    socket.on('message', (message) => {
+      const { type, value } = JSON.parse(message);
+
+      console.log({ type, value });
+    });
+
+    const message = {
+      type: MESSAGE.BLOCKS,
+      value: blocks,
+    };
+    socket.send(JSON.stringify(message));
+  }
+
+  broadcast(type, value) {
+    console.log(`[ws:broadcast] ${type}...`);
+    const message = JSON.stringify({ type, value });
+
+    this.sockets.forEach((socket) => socket.send(message));
   }
 }
 
