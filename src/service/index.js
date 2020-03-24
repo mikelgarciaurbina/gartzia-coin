6204e1420f1fdd430525c363f71571571ae7c157
mikelgarciaurbina <mikelgarciaurbina@gmail.com>
2020-03-24 10:26:34 +0100

    Add first endpoint to get the blocks

--- src/service/index.js
+++ src/service/index.js
@@ -8,7 +8,13 @@ const { HTTP_PORT = 3000 } = process.env;
 const app = express();
 const blockchain = new Blockchain();
 
+blockchain.addBlock('express');
+
 app.use(bodyParser.json());
+
+app.get('/blocks', (req, res) => {
+  res.json(blockchain.blocks);
+});
 
 app.listen(HTTP_PORT, () => {
   console.log(`Service HTTP: ${HTTP_PORT} listening...`);
