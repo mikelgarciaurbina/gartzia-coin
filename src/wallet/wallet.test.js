75f923502b4a172930fcdd03fc8444a082e423c7
mikelgarciaurbina <mikelgarciaurbina@gmail.com>
2020-03-26 10:38:17 +0100

    Add tests to sign function

--- src/wallet/wallet.test.js
+++ src/wallet/wallet.test.js
@@ -13,4 +13,11 @@ describe('Wallet', () => {
     expect(typeof wallet.publicKey).toEqual('string');
     expect(wallet.publicKey.length).toEqual(130);
   });
+
+  it('use sign()', () => {
+    const signature = wallet.sign('h3ll0');
+
+    expect(typeof signature).toEqual('object');
+    expect(signature).toEqual(wallet.sign('h3ll0'));
+  });
 });
