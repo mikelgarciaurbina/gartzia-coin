392d19c713cd87dce687cb54790ce551eb9084e4
mikelgarciaurbina <mikelgarciaurbina@gmail.com>
2020-03-26 10:35:08 +0100

    Add tests to Transaction

---
+++ src/wallet/transaction.test.js
@@ -0,0 +1,52 @@
+import Transaction from './transaction';
+import Wallet from './wallet';
+
+describe('Transaction', () => {
+  let wallet;
+  let transaction;
+  let amount;
+  let recipientAdress;
+
+  beforeEach(() => {
+    wallet = new Wallet();
+    recipientAdress = 'r3c1p13nt';
+    amount = 5;
+    transaction = Transaction.create(wallet, recipientAdress, amount);
+  });
+
+  it('outputs the `amount` subtracted from the wallet balance', () => {
+    const output = transaction.outputs.find(({ address }) => address === wallet.publicKey);
+    expect(output.amount).toEqual(wallet.balance - amount);
+  });
+
+  it('outputs the `amount` added to the recipient', () => {
+    const output = transaction.outputs.find(({ address }) => address === recipientAdress);
+    expect(output.amount).toEqual(amount);
+  });
+
+  describe('Transacting with an amount that exceeds the balance', () => {
+    beforeEach(() => {
+      amount = 500;
+      transaction = undefined;
+    });
+
+    it('does not create the transaction', () => {
+      expect(() => {
+        transaction = Transaction.create(wallet, recipientAdress, amount);
+      }).toThrowError(`Amount: ${amount} exceeds balance.`);
+    });
+  });
+
+  it('inputs the balance of the wallet', () => {
+    expect(transaction.input.amount).toEqual(wallet.balance);
+  });
+
+  it('inputs the sender address of the wallet', () => {
+    expect(transaction.input.address).toEqual(wallet.publicKey);
+  });
+
+  it('inputs has a signature using the wallet', () => {
+    expect(typeof transaction.input.signature).toEqual('object');
+    expect(transaction.input.signature).toEqual(wallet.sign(transaction.outputs));
+  });
+});
