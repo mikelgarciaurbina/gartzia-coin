2c8e6bb2b59c3edcb1b17c6bb77a594e49d66517
mikelgarciaurbina <mikelgarciaurbina@gmail.com>
2020-03-26 10:34:36 +0100

    Add first approach of Transaction class

---
+++ src/wallet/transaction.js
@@ -0,0 +1,32 @@
+import uuidV1 from 'uuid/v1';
+
+class Transaction {
+  constructor() {
+    this.id = uuidV1();
+    this.input = null;
+    this.outputs = [];
+  }
+
+  static create(senderWallet, recipientAdress, amount) {
+    const { balance, publicKey } = senderWallet;
+
+    if (amount > balance) throw Error(`Amount: ${amount} exceeds balance.`);
+
+    const transaction = new Transaction();
+    transaction.outputs.push(...[
+      { amount: balance - amount, address: publicKey },
+      { amount, address: recipientAdress },
+    ]);
+
+    transaction.input = {
+      timestamp: Date.now(),
+      amount: senderWallet.balance,
+      address: senderWallet.publicKey,
+      signature: senderWallet.sign(transaction.outputs),
+    };
+
+    return transaction;
+  }
+}
+
+export default Transaction;
