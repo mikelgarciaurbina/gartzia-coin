05dc2fc7510e593c91e369d3da596d2569cdf99f
mikelgarciaurbina <mikelgarciaurbina@gmail.com>
2020-03-27 08:41:16 +0100

    Add first approach of Miner class

---
+++ src/miner/miner.js
@@ -0,0 +1,15 @@
+class Miner {
+  constructor(blockchain, p2pService, wallet) {
+    this.blockchain = blockchain;
+    this.p2pService = p2pService;
+    this.wallet = wallet;
+  }
+
+  mine() {
+    const {
+      blockchain: { memoryPool },
+    } = this;
+  }
+}
+
+export default Miner;
