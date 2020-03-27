d90a899bcb7a6bbbed6ef7c7976179c19e9fa496
mikelgarciaurbina <mikelgarciaurbina@gmail.com>
2020-03-27 08:54:13 +0100

    Add tests to reward function

--- src/wallet/transaction.test.js
+++ src/wallet/transaction.test.js
@@ -1,5 +1,6 @@
-import Transaction from './transaction';
+import Transaction, { REWARD } from './transaction';
 import Wallet from './wallet';
+import { blockchainWallet } from './index';
 
 describe('Transaction', () => {
   let wallet;
@@ -77,6 +78,22 @@ describe('Transaction', () => {
     it('outputs an amount for the next recipient', () => {
       const output = transaction.outputs.find(({ address }) => address === nextRecipient);
       expect(output.amount).toEqual(nextAmount);
+    });
+  });
+
+  describe('creating a reward transaction', () => {
+    beforeEach(() => {
+      transaction = Transaction.reward(wallet, blockchainWallet);
+    });
+
+    it('reward the miners wallet', () => {
+      expect(transaction.outputs.length).toEqual(2);
+
+      let output = transaction.outputs.find(({ address }) => address === wallet.publicKey);
+      expect(output.amount).toEqual(REWARD);
+
+      output = transaction.outputs.find(({ address }) => address === blockchainWallet.publicKey);
+      expect(output.amount).toEqual(blockchainWallet.balance - REWARD);
     });
   });
 });
